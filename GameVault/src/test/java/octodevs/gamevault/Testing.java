59579e83c0976adc82d56f4819fc67ed9fb3dff3
dju4mota <fernando.b@ges.inatel.br>
2025-01-27 13:40:59 -0300

    Testando testes

---
+++ GameVault/src/test/java/octodevs/gamevault/Testing.java
@@ -0,0 +1,21 @@
+package octodevs.gamevault;
+
+import octodevs.gamevault.controllers.ReviewController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class Testing {
+
+    @Autowired
+    private ReviewController controller;
+
+    @Test
+    void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+
+}
---
+++ GameVault/src/test/java/octodevs/gamevault/crud/RequestTest.java
@@ -0,0 +1,47 @@
+package octodevs.gamevault.crud;
+
+
+import octodevs.gamevault.controllers.ReviewController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class RequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private ReviewController controller;
+
+    @Test
+    void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/reviews",
+                String.class)).contains("[{\"reviewId\":7,\"score\":8,\"comment\":\"Um excelente jogo com gráficos impressionantes, mas um pouco repetitivo.\"," +
+                "\"data\":\"2025-01-18\",\"platform\":\"PlayStation 5\",\"hoursPlayed\":45.0,\"game\":{\"title\":\"Horizon Forbidden West\",\"description\":" +
+                "\"Acompanhe Aloy em uma jornada épica para salvar a Terra de uma nova ameaça.\",\"genre\":\"RPG\"}},{\"reviewId\":12,\"score\":8,\"comment\":" +
+                "\"Um excelente jogo com gráficos impressionantes, mas um pouco repetitivo.\",\"data\":\"2025-01-18\",\"platform\":\"PlayStation 5\",\"hoursPlayed" +
+                "\":45.0,\"game\":{\"title\":\"Horizon Forbidden West\",\"description\":\"Acompanhe Aloy em uma jornada épica para salvar a Terra de uma nova ameaça.\"," +
+                "\"genre\":\"RPG\"}},{\"reviewId\":19,\"score\":8,\"comment\":\"Um excelente jogo com gráficos impressionantes, mas um pouco repetitivo.\",\"data\":null," +
+                "\"platform\":null,\"hoursPlayed\":45.0,\"game\":{\"title\":\"Horizon Forbidden West\",\"description\":\"Acompanhe Aloy em uma jornada épica para salvar a " +
+                "Terra de uma nova ameaça.\",\"genre\":\"RPG\"}},{\"reviewId\":30,\"score\":8,\"comment\":\"Um excelente jogo com gráficos impressionantes, mas um pouco repetitivo." +
+                "\",\"data\":\"\",\"platform\":null,\"hoursPlayed\":45.0,\"game\":{\"title\":\"Horizon Forbidden West\",\"description\":\"Acompanhe Aloy em uma jornada épica para " +
+                "salvar a Terra de uma nova ameaça.\",\"genre\":\"RPG\"}},{\"reviewId\":45,\"score\":7,\"comment\":\"Um excelente jogo com gráficos impressionantes, mas um " +
+                "pouco repetitivo.\",\"data\":\"\",\"platform\":null,\"hoursPlayed\":5.0,\"game\":{\"title\":\"Horizon Forbidden West\",\"description\":\"Acompanhe Aloy em uma " +
+                "jornada épica para salvar a Terra de uma nova ameaça.\",\"genre\":\"RPG\"}},{\"reviewId\":450,\"score\":8,\"comment\":\"Um excelente jogo com gráficos impressionantes," +
+                " mas um pouco repetitivo.\",\"data\":\"\",\"platform\":null,\"hoursPlayed\":45.0,\"game\":{\"title\":\"Horizon Forbidden West\",\"description\":\"Acompanhe Aloy em uma" +
+                " jornada épica para salvar a Terra de uma nova ameaça.\",\"genre\":\"RPG\"}}]");
+    }
+
+
+}
