3c1e4a7f9fa712c9be00aeb155c367e3cd8a4aef
dju4mota <fernando.b@ges.inatel.br>
2025-02-03 20:49:04 -0300

    sql syntax

--- GameVault/BD/start.sql
+++ GameVault/BD/start.sql
@@ -1,5 +1,5 @@
-Insert data into the reviews table
-INSERT INTO reviews (review_id, score, comment, data, hours_played, platform, title, description, genre) VALUES
-(1, 9, 'Um jogo incrível com uma narrativa emocionante!', '2025-01-19', 120.5, 'Nintendo Switch', 'The Legend of Zelda: Breath of the Wild', 'Explore um vasto mundo aberto cheio de mistérios e aventuras.', 'Action-Adventure'),
-(2, 8, 'Um excelente jogo com gráficos impressionantes, mas um pouco repetitivo.', '2025-01-18', 45.0, 'PlayStation 5', 'Horizon Forbidden West', 'Acompanhe Aloy em uma jornada épica para salvar a Terra de uma nova ameaça.', 'RPG'),
-(3, 7, 'Boa jogabilidade, mas com problemas de performance no PC.', '2025-01-15', 30.0, 'PC', 'Cyberpunk 2077', 'Explore uma cidade futurista em um mundo aberto repleto de escolhas.', 'Action RPG');
+use gamevault;
+INSERT INTO review (review_id, score, comment, data, hours_played, platform, title, description, genre) VALUES
+                                                                                                             (1, 9, 'Um jogo incrível com uma narrativa emocionante!', '2025-01-19', 120.5, 'Nintendo Switch', 'The Legend of Zelda: Breath of the Wild', 'Explore um vasto mundo aberto cheio de mistérios e aventuras.', 'Action-Adventure'),
+                                                                                                             (2, 8, 'Um excelente jogo com gráficos impressionantes, mas um pouco repetitivo.', '2025-01-18', 45.0, 'PlayStation 5', 'Horizon Forbidden West', 'Acompanhe Aloy em uma jornada épica para salvar a Terra de uma nova ameaça.', 'RPG'),
+                                                                                                             (3, 7, 'Boa jogabilidade, mas com problemas de performance no PC.', '2025-01-15', 30.0, 'PC', 'Cyberpunk 2077', 'Explore uma cidade futurista em um mundo aberto repleto de escolhas.', 'Action RPG');
