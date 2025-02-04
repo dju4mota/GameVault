5b8bda42bbd7e28c08292c882ae96cc2397f500f
dju4mota <fernando.b@ges.inatel.br>
2025-02-03 21:00:42 -0300

    Criando BD apartir do script e nao mais automatico

--- GameVault/src/main/resources/start.sql
+++ GameVault/src/main/resources/start.sql
@@ -1,4 +1,16 @@
 use gamevault;
+
+CREATE TABLE IF NOT EXISTS reviews (
+    reviewId INT PRIMARY KEY,
+    score INT NOT NULL,
+    comment TEXT,
+    data DATE,
+    hoursPlayed DECIMAL(10, 2),
+    platform VARCHAR(100),
+    gameTitle VARCHAR(255) NOT NULL,
+    gameDescription TEXT,
+    gameGenre VARCHAR(100)
+);
 INSERT INTO review (review_id, score, comment, data, hours_played, platform, title, description, genre) VALUES
                                                                                                              (1, 9, 'Um jogo incrível com uma narrativa emocionante!', '2025-01-19', 120.5, 'Nintendo Switch', 'The Legend of Zelda: Breath of the Wild', 'Explore um vasto mundo aberto cheio de mistérios e aventuras.', 'Action-Adventure'),
                                                                                                              (2, 8, 'Um excelente jogo com gráficos impressionantes, mas um pouco repetitivo.', '2025-01-18', 45.0, 'PlayStation 5', 'Horizon Forbidden West', 'Acompanhe Aloy em uma jornada épica para salvar a Terra de uma nova ameaça.', 'RPG'),
