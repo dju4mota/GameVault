c155cebf9f89cf937f680a3b2bea57cbdac6e1a6
dju4mota <fernando.b@ges.inatel.br>
2025-02-06 12:33:28 -0300

    Criando usuario e dependencias spring security

--- GameVault/src/main/java/octodevs/gamevault/models/User.java
+++ GameVault/src/main/java/octodevs/gamevault/models/User.java
@@ -1,4 +1,18 @@
 package octodevs.gamevault.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class User {
+
+    @Id
+    private Long userId;
+    private String login;
+    private String password;
+
+    public User() {
+    }
 }
