a224d9a811be63354aede36f75ea3fb3f1d48509
dju4mota <fernando.b@ges.inatel.br>
2025-11-30 22:33:13 -0300

    Initial Commit

---
+++ GameVault/src/test/java/br/octodevs/GameVault/GameVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package br.octodevs.GameVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
