9ab025f447bf6e29d38086e809ada8c62b255d37
dju4mota <fernando.b@ges.inatel.br>
2025-12-05 10:35:00 -0300

    Feat: Entidade GameList

--- GameVault/src/main/java/br/octodevs/GameVault/model/gameList/GameList.java
+++ GameVault/src/main/java/br/octodevs/GameVault/model/gameList/GameList.java
@@ -1,5 +1,22 @@
 package br.octodevs.GameVault.model.gameList;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Entity
+@Getter
+@Setter
 public class GameList {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long GameListId;
+    private String name;
+    private ArrayList<Long> gamesList;
 }
